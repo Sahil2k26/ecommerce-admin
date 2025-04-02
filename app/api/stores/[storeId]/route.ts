bdacbc3651ac22fd6175e4723f4bff02c0e4e041
Sahil2k26 <kansalsahil71+git@gmail.com>
2025-04-02 20:13:15 +0530

    added public-store-api-route

--- app/api/stores/[storeId]/route.ts
+++ app/api/stores/[storeId]/route.ts
@@ -2,6 +2,24 @@ import prismadb from "@/lib/prismadb";
 import { auth } from "@clerk/nextjs/server";
 import { NextRequest, NextResponse } from "next/server";
 
+
+export async function GET(req:NextRequest,
+    {params}:{params:Promise<{storeId:string}>}
+) {
+    const {storeId}=await params
+    const store=await prismadb.store.findMany({
+        where:{
+            id:storeId
+        },
+        include:{
+            Billboards:true,
+            Categories:true
+        }
+    })
+    if(!store) return NextResponse.json({message:"Store doesn't exists"},{status:404});
+    return NextResponse.json({message:"found",store});
+    
+}
 export async function PATCH(req:NextRequest,{params}:{params:Promise<{storeId:string}>}) {
     const {userId}=await auth();
     if(!userId){
